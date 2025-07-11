188589e462efccbc5e77b9c21d2ac7cff9809c3f
manjo95 <selva.manoj@gmail.com>
2025-07-11 23:35:58 +0100

    Initial commit

---
+++ src/main/java/com/example/daily_task_tracker/DailyTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.daily_task_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyTaskTrackerApplication.class, args);
+	}
+
+}
